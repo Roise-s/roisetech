88ac7831c2cfc227e4a547423eda39440d5356bc
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-08-28 17:57:20 +0200

    Start 1.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { Routes, Route } from "react-router";
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </>
   )
 }
