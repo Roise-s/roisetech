a4dee3c6f0d7c8174b00ba6a3fefe92b959b0891
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-09-19 16:27:17 +0200

    Update ClientProjects.tsx

--- src/components/ClientProjects.tsx
+++ src/components/ClientProjects.tsx
@@ -10,24 +10,24 @@ const ClientProjects = () => {
             <button type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800 active" id="tabs-with-card-item-1" aria-selected="true" data-hs-tab="#tabs-with-card-1" aria-controls="tabs-with-card-1" role="tab">
             <svg className="shrink-0 hidden sm:block size-7 hs-tab-active:text-blue-600 hs-tab-active:text-blue-500 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 5.5A3.5 3.5 0 0 1 8.5 2H12v7H8.5A3.5 3.5 0 0 1 5 5.5z"/><path d="M12 2h3.5a3.5 3.5 0 1 1 0 7H12V2z"/><path d="M12 12.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 1 1-7 0z"/><path d="M5 19.5A3.5 3.5 0 0 1 8.5 16H12v3.5a3.5 3.5 0 1 1-7 0z"/><path d="M5 12.5A3.5 3.5 0 0 1 8.5 9H12v7H8.5A3.5 3.5 0 0 1 5 12.5z"/></svg>
             <span className="sm:mt-5">
-                <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">All-in-one workspace</span>
-                <span className="hidden lg:block mt-2 text-neutral-200">Create a business, whether you’ve got a fresh idea.</span>
+                <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">Business Websites</span>
+                <span className="hidden lg:block mt-2 text-neutral-200">Simple, professional websites that highlight what your business does.</span>
             </span>
             </button>
 
             <button type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800" id="tabs-with-card-item-2" aria-selected="false" data-hs-tab="#tabs-with-card-2" aria-controls="tabs-with-card-2" role="tab">
             <svg className="shrink-0 hidden sm:block size-7 hs-tab-active:text-blue-600 hs-tab-active:text-blue-500 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m12 14 4-4"/><path d="M3.34 19a10 10 0 1 1 17.32 0"/></svg>
             <span className="sm:mt-5">
-                <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">Automation on a whole new level</span>
-                <span className="hidden lg:block mt-2 text-neutral-200">Use automation to scale campaigns profitably and save time doing it.</span>
+                <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">E-Commerce Websites</span>
+                <span className="hidden lg:block mt-2 text-neutral-200">Fully functional online shops where customers browse products and make secure payments.</span>
             </span>
             </button>
 
             <button type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800" id="tabs-with-card-item-3" aria-selected="false" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
             <svg className="shrink-0 hidden sm:block size-7 hs-tab-active:text-blue-600 hs-tab-active:text-blue-500 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/><path d="M5 3v4"/><path d="M19 17v4"/><path d="M3 5h4"/><path d="M17 19h4"/></svg>
             <span className="sm:mt-5">
-                <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">Solving problems for every team</span>
-                <span className="hidden lg:block mt-2 text-neutral-200">One tool for your company to share knowledge and ship projects.</span>
+                <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">Web Applications with Admin Panel</span>
+                <span className="hidden lg:block mt-2 text-neutral-200">Go beyond a simple website with a powerful web app, so you stay in full control.</span>
             </span>
             </button>
         </nav>
