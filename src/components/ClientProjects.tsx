35d7dbbd4d70bb11788d9e0cdd184dfe3d2bd0f1
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-09-19 19:48:46 +0200

    Update ClientProjects.tsx
    added the handle Click function

--- src/components/ClientProjects.tsx
+++ src/components/ClientProjects.tsx
@@ -1,5 +1,20 @@
 
 const ClientProjects = () => {
+    const handleClick = (value: number) => {
+        if (value === 1) {
+            document.getElementById("tabs-with-card-1")?.classList.remove("hidden");
+            document.getElementById("tabs-with-card-2")?.classList.add("hidden");
+            document.getElementById("tabs-with-card-3")?.classList.add("hidden");
+        } else if (value === 2) {
+            document.getElementById("tabs-with-card-1")?.classList.add("hidden");
+            document.getElementById("tabs-with-card-2")?.classList.remove("hidden");
+            document.getElementById("tabs-with-card-3")?.classList.add("hidden");
+        } else {
+            document.getElementById("tabs-with-card-1")?.classList.add("hidden");
+            document.getElementById("tabs-with-card-2")?.classList.add("hidden");
+            document.getElementById("tabs-with-card-3")?.classList.remove("hidden");
+        }
+    } 
 
   return (
     <>
@@ -7,7 +22,7 @@ const ClientProjects = () => {
         <div className="max-w-[85rem] mt-[20px] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         {/* Tab Nav */}
         <nav className="max-w-6xl mx-auto flex flex-col sm:flex-row gap-y-px sm:gap-y-0 sm:gap-x-4" aria-label="Tabs" role="tablist" aria-orientation="horizontal">
-            <button type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800 active" id="tabs-with-card-item-1" aria-selected="true" data-hs-tab="#tabs-with-card-1" aria-controls="tabs-with-card-1" role="tab">
+            <button onClick={() => handleClick(1)} type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800 active" id="tabs-with-card-item-1" aria-selected="true" data-hs-tab="#tabs-with-card-1" aria-controls="tabs-with-card-1" role="tab">
             <svg className="shrink-0 hidden sm:block size-7 hs-tab-active:text-blue-600 hs-tab-active:text-blue-500 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 5.5A3.5 3.5 0 0 1 8.5 2H12v7H8.5A3.5 3.5 0 0 1 5 5.5z"/><path d="M12 2h3.5a3.5 3.5 0 1 1 0 7H12V2z"/><path d="M12 12.5a3.5 3.5 0 1 1 7 0 3.5 3.5 0 1 1-7 0z"/><path d="M5 19.5A3.5 3.5 0 0 1 8.5 16H12v3.5a3.5 3.5 0 1 1-7 0z"/><path d="M5 12.5A3.5 3.5 0 0 1 8.5 9H12v7H8.5A3.5 3.5 0 0 1 5 12.5z"/></svg>
             <span className="sm:mt-5">
                 <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">Business Websites</span>
@@ -15,7 +30,7 @@ const ClientProjects = () => {
             </span>
             </button>
 
-            <button type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800" id="tabs-with-card-item-2" aria-selected="false" data-hs-tab="#tabs-with-card-2" aria-controls="tabs-with-card-2" role="tab">
+            <button onClick={() => handleClick(2)} type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800" id="tabs-with-card-item-2" aria-selected="false" data-hs-tab="#tabs-with-card-2" aria-controls="tabs-with-card-2" role="tab">
             <svg className="shrink-0 hidden sm:block size-7 hs-tab-active:text-blue-600 hs-tab-active:text-blue-500 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m12 14 4-4"/><path d="M3.34 19a10 10 0 1 1 17.32 0"/></svg>
             <span className="sm:mt-5">
                 <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">E-Commerce Websites</span>
@@ -23,7 +38,7 @@ const ClientProjects = () => {
             </span>
             </button>
 
-            <button type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800" id="tabs-with-card-item-3" aria-selected="false" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
+            <button onClick={() => handleClick(3)} type="button" className="hs-tab-active:bg-gray-100 hs-tab-active:hover:border-transparent w-full flex flex-col text-start focus:outline-hidden p-3 md:p-5 rounded-xl hs-tab-active:bg-neutral-800 hover:bg-neutral-800 focus:bg-neutral-800" id="tabs-with-card-item-3" aria-selected="false" data-hs-tab="#tabs-with-card-3" aria-controls="tabs-with-card-3" role="tab">
             <svg className="shrink-0 hidden sm:block size-7 hs-tab-active:text-blue-600 hs-tab-active:text-blue-500 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/><path d="M5 3v4"/><path d="M19 17v4"/><path d="M3 5h4"/><path d="M17 19h4"/></svg>
             <span className="sm:mt-5">
                 <span className="hs-tab-active:text-blue-600 block font-semibold  hs-tab-active:text-blue-500 text-neutral-200">Web Applications with Admin Panel</span>
@@ -34,7 +49,7 @@ const ClientProjects = () => {
         {/* End Tab Nav */}
 
         {/* Tab Content */}
-        <div className="mt-12 md:mt-16">
+        <div className="mt-12 md:mt-16 justify-center">
             <div id="tabs-with-card-1" role="tabpanel" aria-labelledby="tabs-with-card-item-1">
             {/* Devices */}
             <div className="max-w-[1140px] lg:pb-32 relative">
