d6a7b999f2dcbb5f662c7332172ec9bc0efb0bd3
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-09-20 15:05:55 +0200

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import '../styles/styles.css'
 
 const Footer = () => {
     function getCurrentYear(): number {
@@ -9,12 +10,8 @@ const Footer = () => {
 
   return (
     <footer className=" lg:grid lg:grid-cols-5 bg-gray-900">
-        <div className="relative block h-32 lg:col-span-2 lg:h-full">
-            <img
-            src="https://images.unsplash.com/photo-1642370324100-324b21fab3a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
-            alt=""
-            className="absolute inset-0 h-full w-full object-cover"
-            />
+        <div className="relative block h-32 lg:col-span-2 lg:h-full"> 
+            <div className="container"></div>
         </div>
 
         <div className="px-4 py-16 sm:px-6 lg:col-span-3 lg:px-8">
